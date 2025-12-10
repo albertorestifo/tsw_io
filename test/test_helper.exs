a8790efea8796237862539ec4e87a6db5b71adf1
Alberto Restifo <alberto@restifo.dev>
2025-12-10 21:16:53 +0100

    Add ensure started call

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,6 @@
+# Ensure the application is started before setting up test infrastructure
+{:ok, _} = Application.ensure_all_started(:tsw_io)
+
 Mimic.copy(Req, type_check: true)
 Mimic.copy(TswIo.Train.Identifier)
 Mimic.copy(TswIo.Simulator.Client)
