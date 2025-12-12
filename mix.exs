343c9867034696b84195e05b02cfcafb41b2f4d6
Alberto Restifo <alberto@restifo.dev>
2025-12-12 12:47:16 +0100

    Release 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TswIo.MixProject do
   def project do
     [
       app: :tsw_io,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
